d913701afe8bb6c9b589827844315cd0845e3f5e
KenniCodes <kennedygosnell@outlook.com>
2023-11-02 22:57:14 -0700

    refactor data for topics and photos components

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,30 +1,13 @@
 import React from "react";
 
+import topics from "mocks/topics";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
 const TopicList = () => {
   return (
     <ul className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((sampleTopicData, index) => (
+      {topics.map((sampleTopicData, index) => (
         <TopicListItem key={sampleTopicData.id} sampleTopic={sampleTopicData} />
       ))}
     </ul>
--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -5,7 +5,7 @@ import "../styles/TopicListItem.scss";
 const TopicListItem = ({ sampleTopic }) => {
   return ( 
     <ul className="topic-list__item">
-      <a href={`${sampleTopic.slug}`}><span>{sampleTopic.title}</span></a>
+      <span>{sampleTopic.title}</span>
     </ul>
   );
 };
