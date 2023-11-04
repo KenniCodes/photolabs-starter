80c641244c58cac555ca7f0c69bca1351c274aa1
KenniCodes <kennedygosnell@outlook.com>
2023-11-03 21:56:04 -0700

    refactor useState hooks into useReducer function, also setup useApplicationData to be able to handle other functionality

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,32 +1,62 @@
-import { useState, useCallback } from 'react';
+import { useReducer, useCallback } from 'react';
+
+const initialState = {
+  favouritePhotos: [],
+  clickedPhoto: null,
+  isModalOpen: false,
+};
+
+export const ACTIONS = {
+  TOGGLE_FAVOURITE: 'TOGGLE_FAVOURITE',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  CLOSE_MODAL: 'CLOSE_MODAL',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.TOGGLE_FAVOURITE:
+      return {
+        ...state,
+        favouritePhotos: state.favouritePhotos.includes(action.payload)
+          ? state.favouritePhotos.filter(id => id !== action.payload)
+          : [...state.favouritePhotos, action.payload],
+      };
+    case ACTIONS.SELECT_PHOTO:
+      return {
+        ...state,
+        clickedPhoto: action.payload,
+        isModalOpen: true,
+      };
+    case ACTIONS.CLOSE_MODAL:
+      return {
+        ...state,
+        clickedPhoto: null,
+        isModalOpen: false,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
 
 const useApplicationData = () => {
-  const [favouritePhotos, setFavouritePhotos] = useState([]);
-  const [clickedPhoto, setClickedPhoto] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const openModal = useCallback((photo) => {
-    setClickedPhoto(photo);
-    setIsModalOpen(true);
+    dispatch({ type: ACTIONS.SELECT_PHOTO, payload: photo });
   }, []);
 
   const closeModal = useCallback(() => {
-    setIsModalOpen(false);
-    setClickedPhoto(null);
+    dispatch({ type: ACTIONS.CLOSE_MODAL });
   }, []);
 
   const toggleFavourite = useCallback((photoId) => {
-    setFavouritePhotos(prevFavourites => {
-      return prevFavourites.includes(photoId)
-        ? prevFavourites.filter(id => id !== photoId)
-        : [...prevFavourites, photoId];
-    });
+    dispatch({ type: ACTIONS.TOGGLE_FAVOURITE, payload: photoId });
   }, []);
 
   return {
-    isModalOpen,
-    favouritePhotos,
-    clickedPhoto,
+    isModalOpen: state.isModalOpen,
+    favouritePhotos: state.favouritePhotos,
+    clickedPhoto: state.clickedPhoto,
     openModal,
     closeModal,
     toggleFavourite
